e8e51fc6ec121f959482f93b103e121807ce455e
Ungurean Ana-Cristina <ungureanana-cristina@Ungureans-MacBook-Pro.local>
2023-10-10 23:00:23 +0300

    first commit

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var mixed = [];
+mixed.push('mario');
+mixed.push(25);
+mixed.push(true);
+console.log(mixed);
+var ninja;
+ninja = { name: 'yoshi', age: '12' };
+console.log(ninja);
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+let mixed: any[] = [];
+
+mixed.push('mario');
+mixed.push(25);
+mixed.push(true);
+
+console.log(mixed);
+
+let ninja: { name: any, age: any};
+
+ninja = { name:'yoshi', age:'12'};
+
+console.log(ninja);
