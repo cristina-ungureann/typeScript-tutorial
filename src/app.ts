2c9928c6f532b908193b426ebea4dd692c6872ce
Ungurean Ana-Cristina <ungureanana-cristina@Ungureans-MacBook-Pro.local>
2023-10-18 18:56:15 +0300

    Tuples

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,10 @@
-// enum resourceType {BOOK, AUTHOR, FILM, DIRECTOR, PERSON};
+let arr = ['ana', 25, true];
+arr[0] = false;
+arr[1] = 'tina';
 
-// interface Resource <T> {
-//    uid: number;
-//    resourceType: resourceType,
-//    data: T
-// }
+let tup: [string, number, boolean] = ['ana', 22, true];
 
-// const docOne: Resource <object> = {
-//    uid: 1,
-//    resourceType: resourceType.BOOK,
-//    data:{ title:"Name of wind"}
-// }
+tup[0]= "Criss";
 
-// const docTwo: Resource <object> ={
-//  uid:2,
-//  resourceType: resourceType.PERSON,
-//  data:{name: "Criss"}
-// }
-// console.log(docOne, docTwo);
-
-
-// DAYS OF THE WEEK
-
-
-// enum weekDay { Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday}
-
-
-// function getDayMessage (day: weekDay){
-//    if (day >= weekDay.Monday && day <= weekDay.Friday ){
-//       return "It's a workday"
-//    } else {
-//       return "It's weekend"
-//    }
-// }
-
-enum TrafficLightColor {
-   Red,
-   Yellow,
-   Green,
-}
-
-function getNextLightColor(currentColor: TrafficLightColor): TrafficLightColor {
-   switch (currentColor) {
-       case TrafficLightColor.Red:
-           return TrafficLightColor.Yellow;
-       case TrafficLightColor.Yellow:
-           return TrafficLightColor.Green;
-       case TrafficLightColor.Green:
-           return TrafficLightColor.Red;
-   }
-}
-
-console.log(getNextLightColor(TrafficLightColor.Yellow)); // This should correctly return TrafficLightColor.Red
-
+let student: [ string, number] ;
+student = ['Ken', 20];
